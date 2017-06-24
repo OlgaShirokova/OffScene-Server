bc3ffa9b34f5a3eac13b1acabdefbd18838b8ce4
aerox <aerox.javascript@hotmail.com>
2017-06-24 09:54:58 +0200

    feat: add routes

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
 import logger from 'koa-logger';
+import routes from '~/routes';
+import { errorHandling } from '~/middlewares';
 
-new Koa().use(koaBody()).use(logger()).listen(3000);
+new Koa()
+  .use(koaBody())
+  .use(logger())
+  .use(errorHandling)
+  .use(routes)
+  .listen(3000);
