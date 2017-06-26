201474873a78c692c8c5c1604fab794d1945070a
aerox <aerox.javascript@hotmail.com>
2017-06-26 18:15:12 +0200

    feat: add userInfo controller function

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,38 @@
+import db from '~/models';
+import { userInfoSelector } from '~/selectors/user';
+const { User, Calendar, AwayDay, MusicGenre } = db;
+
 async function events(ctx) {
   ctx.body = 'events';
 }
 
 async function userInfo(ctx) {
-  ctx.body = 'user-info';
+  const { id: djId } = ctx.params;
+  const attributes = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+  ];
+  const user = await User.findById(djId, {
+    include: [{ model: Calendar, attributes }, AwayDay],
+  });
+
+  if (!user || user.get().role === 1) {
+    // user does not exist or is an organizer
+    ctx.throw(400, 'Not Authorized');
+  }
+
+  const genres = await db.connection.models.djGenres.findAll({
+    where: { userId: djId },
+  });
+
+  user.dataValues.genres = genres ? genres : [];
+
+  ctx.body = userInfoSelector(user.get());
 }
 
 async function profile(ctx) {
