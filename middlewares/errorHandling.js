1a9c2c2bcf577bddb8918b808dc4672be1ed1683
aerox <aerox.javascript@hotmail.com>
2017-06-26 14:44:01 +0200

    refactor: simplify errorHandling middleware

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,17 +1,8 @@
 export default async function errorHandling(ctx, next) {
   try {
     await next();
-  } catch (err) {
-    ctx.body = undefined;
-    switch (ctx.status) {
-      case 401:
-        ctx.app.emit('error', err, this);
-        break;
-      default:
-        if (err.message) {
-          ctx.body = { errors: [err.message] };
-        }
-        ctx.app.emit('error', err, this);
-    }
+  } catch ({ message, status }) {
+    ctx.status = status;
+    ctx.body = { errors: [message] };
   }
 }
