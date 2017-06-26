dac154c68ba085573fdbaa59ab7b3ad6f83816ce
aerox <aerox.javascript@hotmail.com>
2017-06-26 18:14:12 +0200

    feat: create additional selectors for user

--- selectors/user.js
+++ selectors/user.js
@@ -1,10 +1,45 @@
 import { encodeJwt } from '~/utils/jwt';
 
-export function signInSelector(user, token) {
-  const { password, bankAccount, swift, ...userInfo } = user;
+export function signInSelector(user) {
+  const {
+    password,
+    bankAccount,
+    swift,
+    createdAt,
+    updatedAt,
+    ...userInfo
+  } = user;
 
   return {
     ...userInfo,
     authToken: encodeJwt(userInfo.id),
   };
+}
+
+export function userInfoSelector(user) {
+  let {
+    password,
+    bankAccount,
+    swift,
+    email,
+    staff,
+    createdAt,
+    updatedAt,
+    role,
+    ...userInfo
+  } = user;
+
+  userInfo.awayDays = awayDaysSelector(userInfo.awayDays);
+
+  userInfo.genres = genresSelector(userInfo.genres);
+
+  return userInfo;
+}
+
+export function awayDaysSelector(awayDays) {
+  return awayDays.map(({ date }) => date);
+}
+
+export function genresSelector(genres) {
+  return genres.map(({ musicGenreName }) => musicGenreName);
 }
