a4f90d465276db9869a662e317d3356c723847f4
aerox <aerox.javascript@hotmail.com>
2017-06-26 14:42:24 +0200

    refactor: delete non used code in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,9 @@
-async function signIn(ctx) {
-  ctx.body = 'sign-in';
-}
-
 async function events(ctx) {
   ctx.body = 'events';
 }
 
 async function userInfo(ctx) {
   ctx.body = 'user-info';
-}
-
-async function signUp(ctx) {
-  ctx.body = 'sign-up';
 }
 
 async function profile(ctx) {
@@ -26,22 +18,20 @@ async function blockUser(ctx) {
   ctx.body = 'block-user';
 }
 
-const away = {
-  async post(ctx) {
-    ctx.body = 'away-post';
-  },
-  async delete(ctx) {
-    ctx.body = 'away-delete';
-  },
-};
+async function postAway(ctx) {
+  ctx.body = 'away-post';
+}
+
+async function deleteAway(ctx) {
+  ctx.body = 'away-delete';
+}
 
 export default {
-  signIn,
   events,
   userInfo,
-  signUp,
   profile,
   profilePicture,
   blockUser,
-  away,
+  postAway,
+  deleteAway,
 };
