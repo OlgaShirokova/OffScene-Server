a0c12805c6a903b275855c2736ecf0576e398e21
Olga Shirokova <olga.shirokova17@gmail.com>
2017-06-30 18:36:20 +0200

    feat: app controller for music genres, music genre initial filling and seed file

--- controllers/app.js
+++ controllers/app.js
@@ -1,5 +1,21 @@
+import db from '~/models';
+const { MusicGenre } = db;
+
 async function genres(ctx) {
-  ctx.body = 'genres';
+  const inputField = ctx.request.url;
+  const query = inputField.split('=')[1];
+
+  try {
+    console.log(query);
+    const genres = await MusicGenre.findAll({
+      where: {
+        name: { $like: '%' + query + '%' },
+      },
+    });
+    ctx.body = genres.map(el => el.name);
+  } catch (err) {
+    ctx.throw(500, 'Service not Available');
+  }
 }
 
 export default {
